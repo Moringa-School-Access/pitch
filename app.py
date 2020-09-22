e85faa1650554c3b92935889f41b86f75b567258
Alexotieno1717 <alexotieno900@gmail.com>
2020-09-23 01:55:02 +0300

    modified app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_script import Manager, Shell, Server
 from flask_migrate import Migrate, MigrateCommand
 
 # Creating app instance
-app = create_app('development')
+app = create_app('production')
 
 manager = Manager(app)
 migrate = Migrate(app, db)
